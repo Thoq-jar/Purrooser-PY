1e2fc722212b6e26bafedc5ae4db2238421df624
Tristan <tristanshaw000@gmail.com>
2024-08-16 05:48:48 -0400

    oops

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 ### Installation
 ```bash
+echo "Setting up..."
+cd ~
+echo "Cleaning..."
+rm -rf .purrooser
+mkdir .purrooser
+cd .purrooser
+echo "Downloading..."
+git clone https://github.com/Thoq-jar/Purrooser.git
+echo "Loading..."
+cd Purrooser
 chmod +x install.sh
 ./install.sh
 ```
--- install.sh
+++ install.sh
@@ -1,19 +1,5 @@
 echo "Installing Purrooser..."
 
-echo "Setting up..."
-cd ~
-
-echo "Cleaning..."
-rm -rf .purrooser
-mkdir .purrooser
-cd .purrooser
-
-echo "Downloading..."
-git clone https://github.com/Thoq-jar/Purrooser.git
-
-echo "Loading..."
-cd Purrooser
-
 echo "Preparing..."
 chmod +x configure
 chmod +x Thoqfile
